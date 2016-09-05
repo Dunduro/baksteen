d111d31fefa0292d73dadf0d50bfd1eb0d2c7dad
elmar <elmar.van.wijnen@webengine.nl>
2016-09-05 14:36:15 +0200

    added functionality

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class DefaultController extends Controller
 {
@@ -19,13 +20,18 @@ class DefaultController extends Controller
     public function uploadAction(Request $request)
     {
         $files = $request->files->all();
-        foreach($files as $no=>$file){
+        $destination = $this->getParameter('image_location');
+        /**
+         * @var int $no
+         * @var UploadedFile $file
+         */
+        foreach($files as $no=>$file)
+        {
+
             $methods = get_class_methods($file);
-            $data = array(
-                ''
-            );
+            $file->move($destination,time().'_'.$file->getClientOriginalName());
+
         }
-        $time = time();
         $response = new JsonResponse();
         $response->setData(array(
             'data' => 123,
